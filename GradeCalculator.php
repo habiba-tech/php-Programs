cd771888fb78ab7865e84139241bae6415d344af
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-10-18 13:31:58 +0530

    Create GradeCalculator.php

---
+++ GradeCalculator.php
@@ -0,0 +1,15 @@
+<?php
+$marks = 82;
+
+if ($marks >= 90) {
+    echo "Grade: A+";
+} elseif ($marks >= 75) {
+    echo "Grade: A";
+} elseif ($marks >= 60) {
+    echo "Grade: B";
+} elseif ($marks >= 40) {
+    echo "Grade: C";
+} else {
+    echo "Fail";
+}
+?>
