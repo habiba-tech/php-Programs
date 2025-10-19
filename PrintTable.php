95abc1b54237744abdeeb37e396fd7f3cdc6672e
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-10-20 02:16:27 +0530

    Create PrintTable.php

---
+++ PrintTable.php
@@ -0,0 +1,9 @@
+<?php
+$number = 5; // you can change this or take from user input
+
+echo "Multiplication Table of $number <br>";
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "$number x $i = " . ($number * $i) . "<br>";
+}
+?>
